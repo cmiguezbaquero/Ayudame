60b3c8a61cd3a10bf17671e5cef0fc70fb9f7e27
Carolina Miguez Baquero <carolinamiguezbaquero@macbook-air-de-carolina-2.home>
2025-09-30 02:17:54 +0200

    AYUDAMEE

--- src/Main.java
+++ src/Main.java
@@ -2,36 +2,53 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
     }
-
 
     // 1. Calcula la velocidad orbital circular (en km/s)
     public static double velocidadOrbital(double radioKm, double masaPlanetaKg) {
         final double G = 6.67430e-20; // Constante G en km^3/(kg*s^2)
-        return 0;
+        return (G * masaPlanetaKg / radioKm);
     }
 
     // 2. Calcula el empuje de un motor (F = m_dot * v_e)
     public static double calcularEmpuje(double flujoMasicoKgPorS, double velocidadEscapeMPorS) {
-        return 0;
+        return (flujoMasicoKgPorS * velocidadEscapeMPorS);
     }
 
     // 3. Convierte nombre de planeta en su número de órbita desde el Sol
     public static int numeroOrbita(String planeta) {
-       return 0;
 
+        switch (planeta.length()) {
+            case "Tierra":
+                return 3;
+            case "Marte":
+                return 4;
+            case "Plutón":
+                return -1;
+            default:
+                return 0;
+        }
     }
 
     // 4. Promedio de altitudes de satélites
     public static double promedioAltitudes(double[] altitudesKm) {
-       return 0;
+
+        double suma = 0;
+
+        for (int i = 0; i < altitudesKm.length; i++) {
+            suma += altitudesKm[i];
+            return suma / altitudesKm.length;
+        }
     }
 
-    // 5. Convierte segundos a formato horas:minutos:segundos (útil para misiones espaciales)
-    public static String convertirTiempo(int segundos) {
+        // 5. Convierte segundos a formato horas:minutos:segundos (útil para misiones espaciales)
+        public static String convertirTiempo (int segundos){
 
-        //return String.format("%02d:%02d:%02d", h, m, s);
-        return "";
+            int horas = segundos / 3600;
+            int minutos = (segundos % 3600) / 60;
+            int segundosRestantes = segundos % 60;
+
+            return String.format("%02d:%02d:%02d", horas, minutos, segundosRestantes);
+        }
     }
-}
+
